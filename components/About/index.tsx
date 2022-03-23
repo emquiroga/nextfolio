043b1df252282ec9cd005b2faa98b5899007187c
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-23 15:32:08 -0300

    Updated info method

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -1,4 +1,5 @@
 import SectionLayout from "../SectionLayout";
+import { about } from "../../_content/info.json";
 
 import styles from "./index.module.css";
 
@@ -12,23 +13,13 @@ const AboutSection = () => {
       <div
         className={`w-full max-w-md px-10 text-textSecondary dark:text-dtextSecondary ${styles["about-info-container"]}`}
       >
-        <p className="my-4 font-primary text-small">
-          Vivo en la ciudad de La Plata, Buenos Aires, Argentina. Soy un apasionado por los
-          lenguajes, la tecnología y la comunicación.
-        </p>
-        <p className="my-4 font-primary text-small">
-          Actualmente trabajo como Jr. Developer en Andreani Grupo Logístico y también como Web
-          Developer en modalidad freelance.
-        </p>
-        <p className="my-4 font-primary text-small">
-          Mi fuerte es el desarrollo frontend con React y el maquetado web. Me encanta el diseño
-          pixel-perfect, las buenas prácticas de código y los diseños responsivos.
-        </p>
-        <p className="my-4 font-primary text-small">
-          Algunos de los lenguajes, librerías y frameworks con los que he trabajado son:
-          JavaScript(ES6)/TypeScript, React (Next.js), Redux, react-router, HTML, CSS/Sass,
-          Bootstrap/Tailwind, Laravel, .NET(C#) y MySQL.
-        </p>
+        {about.map((item, index) => {
+          return (
+            <p key={index} className="my-4 font-primary text-small">
+              {item}
+            </p>
+          );
+        })}
       </div>
     </SectionLayout>
   );
