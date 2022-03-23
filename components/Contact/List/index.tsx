94fb88e7a42335e029ebfa64d97cf6eb10265a5a
Emiliano <emiliano.quiroga093@gmail.com>
2022-03-23 15:31:09 -0300

    removed CV form list

--- components/Contact/List/index.tsx
+++ components/Contact/List/index.tsx
@@ -116,30 +116,6 @@ const ContactList = () => {
             </svg>
           </a>
         </li>
-        <li className={styles["list-item"]}>
-          <a
-            className={styles["list-link"]}
-            href="https://drive.google.com/file/d/16POk_AXtASMkPffgbcOuLtgwukVR1VAl/view"
-            rel="noreferrer"
-            target="_blank"
-            title="Curriculum Vitae"
-          >
-            <svg
-              aria-hidden="true"
-              data-icon="folder"
-              data-prefix="fas"
-              focusable="false"
-              role="img"
-              viewBox="0 0 512 512"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M464 128H272l-64-64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48 48 48h416c26.51 0 48-21.49 48-48V176c0-26.51-21.49-48-48-48z"
-                fill="currentColor"
-              />
-            </svg>
-          </a>
-        </li>
       </ul>
     </div>
   );
